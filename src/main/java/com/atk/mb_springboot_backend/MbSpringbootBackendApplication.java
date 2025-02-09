872214c4b0ea2338e6852f9a55f03dd4a43a844f
Alvaro Kieling <atkwaves@gmail.com>
2025-02-09 14:36:03 -0300

    chore: configuração inicial do projeto

---
+++ src/main/java/com/atk/mb_springboot_backend/MbSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.atk.mb_springboot_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbSpringbootBackendApplication.class, args);
+	}
+
+}
